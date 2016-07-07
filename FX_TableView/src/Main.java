561cf1d80f53f8a942aa1f24d514ee42ac049e8b
Mark Caruso <kazoo6723@gmail.com>
2016-07-07 17:01:40 -0400

    Added getProducts() method to Main.java

--- FX_TableView/src/Main.java
+++ FX_TableView/src/Main.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -25,4 +27,20 @@ public class Main extends Application{
 		window.show();
 	}
 
+	//Get all the products, usually some kind of DB
+	public ObservableList<Products> getProduct(){
+		
+		ObservableList<Products> products = FXCollections.observableArrayList();
+		products.add(new Products("Gibson SG", 1500.00, 20));
+		products.add(new Products("Gibson Les Paul", 2500.00, 10));
+		products.add(new Products("Epiphone EX", 900.99, 40));
+		products.add(new Products("Martin D100", 3050.67, 15));
+		products.add(new Products("Martin D18", 4500.00, 8));
+		
+		return products; 
+	}
+	
+	
+	
+	
 }
