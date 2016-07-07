b213fc9e348946b43ab7267ec7eeeb3d10aaf0dc
Mark Caruso <kazoo6723@gmail.com>
2016-07-07 16:47:22 -0400

    Modified Main.java - set up Stage, layout, and Scene

--- FX_TableView/src/Main.java
+++ FX_TableView/src/Main.java
@@ -1,9 +1,28 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Main {
-
+public class Main extends Application{
+	
+	Stage window;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage; 
+		window.setTitle("FX TableView Demo");
+		
+		VBox layout = new VBox(10);
+		layout.getChildren().addAll();
+		
+		Scene scene = new Scene(layout, 300, 400);
+		window.setScene(scene);
+		window.show();
 	}
 
 }
