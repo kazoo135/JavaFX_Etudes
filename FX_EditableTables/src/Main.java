1e0f5e45540b16c3aa8da39bc4217e7d3f96b4ec
Mark Caruso <kazoo6723@gmail.com>
2016-07-09 14:55:43 -0400

    Initial commit on Editable Tables. Set upt starting point wich is the completed version of FX_TableView project

---
+++ FX_EditableTables/src/Main.java
@@ -0,0 +1,74 @@
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	Stage window;
+	TableView<Products> productsTable;
+	
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage; 
+		window.setTitle("FX TableView Demo");
+		
+		// set up columns for table
+		//name column
+		TableColumn<Products, String> nameCol = new TableColumn<>("Name");
+		nameCol.setMinWidth(200);
+		
+		//use this data for this column
+		nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		//Price col
+		TableColumn<Products, Double> priceCol = new TableColumn<>("Price");
+		priceCol.setMinWidth(100);
+		priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+	
+		//Quantity Col
+		TableColumn<Products, Integer> quantityCol = new TableColumn<>("Quantity");
+		quantityCol.setMinWidth(100);
+		quantityCol.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+		
+		//build the table
+		productsTable = new TableView<>();
+		productsTable.setItems(getProducts());
+		productsTable.getColumns().addAll(nameCol, priceCol, quantityCol);
+		
+		VBox layout = new VBox(10);
+		layout.getChildren().addAll(productsTable);
+		
+		Scene scene = new Scene(layout, 350, 400);
+		window.setScene(scene);
+		window.show();
+	}
+
+	//Get all the products, usually some kind of DB
+	public ObservableList<Products> getProducts(){
+		
+		ObservableList<Products> products = FXCollections.observableArrayList();
+		products.add(new Products("Gibson SG", 1500.00, 20));
+		products.add(new Products("Gibson Les Paul", 2500.00, 10));
+		products.add(new Products("Epiphone EX", 900.99, 40));
+		products.add(new Products("Martin D100", 3050.67, 15));
+		products.add(new Products("Martin D18", 4500.00, 8));
+		
+		return products; 
+	}
+	
+	
+	
+	
+}
