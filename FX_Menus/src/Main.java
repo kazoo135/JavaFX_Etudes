2984d8dda1f5299d0e75118ccfc86aa2468038d9
Mark Caruso <kazoo6723@gmail.com>
2016-07-11 00:52:57 -0400

    Added actions to menu items, added edit menu. Learned how to add alt functionality and to disable a menu item

--- FX_Menus/src/Main.java
+++ FX_Menus/src/Main.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -16,7 +17,7 @@ public class Main extends Application{
 
 	}
 
-	@Override
+	@Override 
 	public void start(Stage primaryStage) throws Exception {
 		window = primaryStage;
 		window.setTitle("FX Menu Demo");
@@ -24,13 +25,46 @@ public class Main extends Application{
 		Menu fileMenu = new Menu("File");
 		
 		//menu items
-		fileMenu.getItems().add(new MenuItem("New"));
-		fileMenu.getItems().add(new MenuItem("Open File"));
-		fileMenu.getItems().add(new MenuItem("Close"));
+		MenuItem newFile = new MenuItem("New");
+		newFile.setOnAction( e -> System.out.println("Creat new file"));
+		fileMenu.getItems().add(newFile);
+		
+		MenuItem openFile = new MenuItem("Open File");
+		openFile.setOnAction( e -> System.out.println("Open a File"));
+		fileMenu.getItems().add(openFile);
+		//add a category separator
+		fileMenu.getItems().add(new SeparatorMenuItem());
+		
+		//close options
+		MenuItem closeFile = new MenuItem("Close");
+		closeFile.setOnAction( e -> System.out.println("Close a file" ));
+		fileMenu.getItems().add(closeFile);
+		fileMenu.getItems().add(new MenuItem("Close All"));
+		fileMenu.getItems().add(new SeparatorMenuItem());
+		
+		//Save options
+		fileMenu.getItems().add(new MenuItem("Save"));
+		fileMenu.getItems().add(new MenuItem("Save As..."));
+		fileMenu.getItems().add(new MenuItem("Save All"));
+		fileMenu.getItems().add(new MenuItem("Revert"));
+		
+		//edit menu
+		Menu editMenu = new Menu("_Edit");
+		//add items to edit menue
+		editMenu.getItems().add(new MenuItem("Cut"));
+		editMenu.getItems().add(new MenuItem("Copy"));
+		editMenu.getItems().add(new MenuItem("Paste"));
+		
+		MenuItem redo = new MenuItem("Redo");
+		redo.setOnAction( e -> System.out.println("Do Over"));
+		redo.setDisable(true);
+		editMenu.getItems().add(redo);
+		
+		
 		
 		//Main menu bar
 		MenuBar menubar = new MenuBar();
-		menubar.getMenus().add(fileMenu);
+		menubar.getMenus().addAll(fileMenu, editMenu);
 		
 		layout = new BorderPane();
 		layout.setTop(menubar);
