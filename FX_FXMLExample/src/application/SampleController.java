799c9e1e753d877013939d88ee8f7794ea653e41
Mark Caruso <kazoo6723@gmail.com>
2016-07-12 20:41:18 -0400

    Added handleButtonClicks() to controller and ran app. Working properly

--- FX_FXMLExample/src/application/SampleController.java
+++ FX_FXMLExample/src/application/SampleController.java
@@ -1,10 +1,19 @@
 package application;
 
+import javafx.scene.control.Button;
+
 public class SampleController {
+	
+	public Button button;
 	
 	//default no args constructor
 	public SampleController(){
 		
+	}
+	
+	public void handleButtonClicks(){
+		System.out.println("Running code from controller");
+		button.setText("I've been clicked");
 	}
 	
 	
