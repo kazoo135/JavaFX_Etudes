4f8981682d3fec23a1711827e1f5134088d57b37
Mark Caruso <kazoo6723@gmail.com>
2016-07-12 22:55:20 -0400

    Added Initializer and Reusable Objects with fx:define in fxml file

--- FX_FXMLInitializeAndBinding/src/application/SampleController.java
+++ FX_FXMLInitializeAndBinding/src/application/SampleController.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
 
-public class SampleController {
+public class SampleController implements Initializable{
 	
 	public Button button;
 	public Label firstLabel; 
+	public ToggleGroup toggle;
 	
 	//default no args constructor
 	public SampleController(){
@@ -16,6 +22,12 @@ public class SampleController {
 	public void handleButtonClicks(){
 		System.out.println("Running code from controller");
 		button.setText("I've been clicked");
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		System.out.println("Loading user data");
+		
 	}
 	
 	
